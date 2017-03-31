d7abc10333dd53f377ddde283364aa34ac57026e
Flier Lu <flier.lu@gmail.com>
2017-03-31 11:18:55 +0800

    remove unused errors

--- zipkin/src/errors.rs
+++ zipkin/src/errors.rs
@@ -8,15 +8,3 @@ error_chain! {
         PoisonError
     }
 }
-
-impl<T> From<::std::sync::mpsc::SendError<T>> for Error {
-    fn from(_: ::std::sync::mpsc::SendError<T>) -> Self {
-        ErrorKind::SendError.into()
-    }
-}
-
-impl<T> From<::std::sync::PoisonError<T>> for Error {
-    fn from(_: ::std::sync::PoisonError<T>) -> Self {
-        ErrorKind::PoisonError.into()
-    }
-}
