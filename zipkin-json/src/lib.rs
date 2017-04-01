59fb71ca4fe8c3ff5414f973a740eb0d67a128a4
Flier Lu <flier.lu@gmail.com>
2017-04-01 12:24:32 +0800

    always enable tokio-io supports

--- zipkin-json/src/lib.rs
+++ zipkin-json/src/lib.rs
@@ -2,6 +2,8 @@
 extern crate error_chain;
 extern crate serde_json;
 extern crate base64;
+extern crate bytes;
+extern crate tokio_io;
 extern crate zipkin;
 
 #[cfg(test)]
@@ -11,17 +13,8 @@ extern crate diff;
 
 mod errors;
 mod encode;
+mod codec;
 
 pub use encode::{to_json, to_string, to_string_pretty, to_vec, to_vec_pretty, to_writer,
                  to_writer_pretty};
-
-#[cfg(feature = "future")]
-extern crate tokio_io;
-#[cfg(feature = "future")]
-extern crate bytes;
-
-#[cfg(feature = "future")]
-mod codec;
-
-#[cfg(feature = "future")]
 pub use codec::JsonCodec;
--- zipkin-thrift/src/lib.rs
+++ zipkin-thrift/src/lib.rs
@@ -4,6 +4,8 @@ extern crate byteorder;
 extern crate ordered_float;
 extern crate thrift;
 extern crate try_from;
+extern crate bytes;
+extern crate tokio_io;
 extern crate zipkin;
 
 #[cfg(test)]
@@ -12,16 +14,7 @@ extern crate chrono;
 mod core;
 mod errors;
 mod encode;
-
-pub use encode::{to_thrift, to_vec, to_writer};
-
-#[cfg(feature = "future")]
-extern crate tokio_io;
-#[cfg(feature = "future")]
-extern crate bytes;
-
-#[cfg(feature = "future")]
 mod codec;
 
-#[cfg(feature = "future")]
+pub use encode::{to_thrift, to_vec, to_writer};
 pub use codec::ThriftCodec;
